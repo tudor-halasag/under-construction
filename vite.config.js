ad719ae3f507ea54f61ccf24c486b44c94402e20
Tudor <tudor.halasag@gmail.com>
2026-03-22 03:05:52 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// base './' ensures assets resolve correctly on GitHub Pages
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+})
